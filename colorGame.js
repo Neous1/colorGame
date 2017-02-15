377dd42596a515d6aa51e3a2caba3d6a0ac19198
Yvon <Yvon@Yvons-iMac.local>
2017-02-15 14:31:46 -0500

    refactored codes remove easy and hard buttons from .js added a reset function to be call by new modebuttons selector

--- colorGame.js
+++ colorGame.js
@@ -8,54 +8,51 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset"); 
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.getElementById("hardBtn");
+//var easyBtn = document.querySelector("#easyBtn");
+//var hardBtn = document.getElementById("hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
-easyBtn.addEventListener("click", function(){
-    hardBtn.classList.remove("selected");
-    easyBtn.classList.add("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    // change the color of the squares 
-    for (var i = 0; i < squares.length; i++){
-        if (colors[i]){ 
-            squares[i].style.background = colors[i];
-        }
-        else{
-            squares[i].style.display = "none";
-        }
-    }
-});
-hardBtn.addEventListener("click", function(){
-    hardBtn.classList.add("selected");
-    easyBtn.classList.remove("selected");
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    // change the color of the squares 
-    for (var i = 0; i < squares.length; i++){
-        squares[i].style.background = colors[i];
-        squares[i].style.display = "block";
-    }
-});
+for (var i =0; i<modeButtons.length; i++){
+    modeButtons[i].addEventListener("click", function(){
+        //remove the selected class from all buttons.
+        modeButtons[0].classList.remove("selected")
+        modeButtons[1].classList.remove("selected")
+        //add the selected class to the selected button
+        this.classList.add("selected");
+        //figure out how many square to show
+        this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+        // pick new color
+        // pick a new pickedColor
+        //update page to reflect change
+        
+        reset();
+        
+    });
+}
 
-resetButton.addEventListener("click", function(){
+function reset(){
     //generate all new colors
     colors = generateRandomColors(numSquares);
     // pick a new random color from arr
     pickedColor = pickColor();
     //change colorDisplay to match pickedColor
     colorDisplay.textContent = pickedColor;
-    this.textContent = "New Colors";
+    resetButton.textContent = "New Colors";
     messageDisplay.textContent = "";
     //change color of squares
     for (var i= 0; i < squares.length; i++){
-        squares[i].style.background = colors[i];
+        squares[i].style.display ="block"; // this line ensures that all 6 colors are displayed before then easy mode romoves 3.
+        if (colors[i]){
+            squares[i].style.background = colors[i];            
+        }
+        else{
+            squares[i].style.display ="none";
+        }
     }
     h1.style.background ="steelblue";
+}
+resetButton.addEventListener("click", function(){
+    reset()
 });
 
 colorDisplay.textContent = pickedColor;
